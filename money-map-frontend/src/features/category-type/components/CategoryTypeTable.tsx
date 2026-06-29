2287fbc9cdd1d591aaa588378023bfc95ff4336d
AlainMateo01 <alainmateo01@gmail.com>
2026-06-28 22:01:52 -0500

    refactor: add CategoryTypeTable component and integrate it into CategoryTypesPage

---
+++ money-map-frontend/src/features/category-type/components/CategoryTypeTable.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import Table from '../../../components/common/Table';
+import type { DropdownOptionDTO } from '../../../types/services/common/DropdownOptionDTO';
+import { categoryTypeService } from '../services/categoryTypeService';
+
+const CategoryTypeTable = () => {
+    const [categoryTypes, setCategoryTypes] = useState<DropdownOptionDTO[]>([]);
+
+  const columns = [
+    { key: 'name', label: 'Name' }
+  ];
+
+  useEffect(() => {
+    const fetchCategoryTypes = async () => {
+      const data = await categoryTypeService.getCategoryTypeDropDownOptions();
+      setCategoryTypes(data);
+    }
+
+    fetchCategoryTypes();
+  }, []);
+
+  return (
+    <div>
+      <Table
+        columns={columns}
+        data={categoryTypes}
+      />
+    </div>
+  );
+}
+
+export default CategoryTypeTable
--- money-map-frontend/src/features/category-type/pages/CategoryTypesPage.tsx
+++ money-map-frontend/src/features/category-type/pages/CategoryTypesPage.tsx
@@ -1,31 +1,11 @@
-import { useEffect, useState } from 'react';
-import Table from '../../../components/common/Table';
-import type { DropdownOptionDTO } from '../../../types/services/common/DropdownOptionDTO';
-import { categoryTypeService } from '../services/categoryTypeService';
+import CategoryTypeTable from "../components/CategoryTypeTable";
 
 const CategoryTypesPage = () => {
-  const [categoryTypes, setCategoryTypes] = useState<DropdownOptionDTO[]>([]);
-
-  const columns = [
-    { key: 'name', label: 'Name' }
-  ];
-
-  useEffect(() => {
-    // Fetch category types from an API or service
-    const fetchCategoryTypes = async () => {
-      const data = await categoryTypeService.getCategoryTypeDropDownOptions();
-      setCategoryTypes(data);
-    }
-
-    fetchCategoryTypes();
-  }, []);
-
   return (
     <div>
-      <Table
-        columns={columns}
-        data={categoryTypes}
-      />
+      <main>
+        <CategoryTypeTable />
+      </main>
     </div>
   );
 };
