8cc109503aed0d1865e2b24d27d11d83e361e020
AlainMateo01 <alainmateo01@gmail.com>
2026-06-29 22:31:21 -0500

    feat:  update service methods of categories to get basic information

--- money-map-frontend/src/features/category-type/services/categoryTypeService.ts
+++ money-map-frontend/src/features/category-type/services/categoryTypeService.ts
@@ -1,5 +1,5 @@
 import { moneyMapClient } from "../../../services/api/clients/moneyMapClient";
-import type { AddCategoryTypeDTO } from "../../../types/services/category-type/AddCategoryTypeDTO";
+import type { AddCategoryTypeDTO } from "../../../types/services/category-type/request/AddCategoryTypeDTO";
 import type { DropdownOptionDTO } from "../../../types/services/common/DropdownOptionDTO";
 
 const BASE_URL = '/CategoryType';
--- money-map-frontend/src/features/category/services/categoryService.ts
+++ money-map-frontend/src/features/category/services/categoryService.ts
@@ -1,17 +1,24 @@
 import { moneyMapClient } from "../../../services/api/clients/moneyMapClient";
-import type { AddCategoryDTO } from "../../../types/services/category/AddCategoryDTO";
+import type { AddCategoryDTO } from "../../../types/services/category/request/AddCategoryDTO";
+import type { GetCategoryBasicInfoDTO } from "../../../types/services/category/response/GetCategoryBasicInfoDTO";
 import type { DropdownOptionDTO } from "../../../types/services/common/DropdownOptionDTO";
 
 const BASE_URL = '/Category';
 
-export const categoryTypeService = {
-  async addCategoryType(category: AddCategoryDTO): Promise<void> {
+export const categoryService = {
+  async addCategory(category: AddCategoryDTO): Promise<void> {
     const endPoint = `${BASE_URL}/AddCategory`;
     await moneyMapClient.post(endPoint, category);
   },
 
   async getCategoryTypeDropDownOptions(): Promise<DropdownOptionDTO[]> {
     const endPoint = `${BASE_URL}/GetCategoryDropDownOptions`;
+    const response = await moneyMapClient.get(endPoint);
+    return response.data.data;
+  },
+
+  async getCategoryBasicInfo(): Promise<GetCategoryBasicInfoDTO[]> {
+    const endPoint = `${BASE_URL}/GetCategoriesBasicInfo`;
     const response = await moneyMapClient.get(endPoint);
     return response.data.data;
   }
