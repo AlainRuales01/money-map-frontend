56b4871e74be4dbd82ac5b70e241e08e162c0bac
AlainMateo01 <alainmateo01@gmail.com>
2026-07-12 22:27:30 -0500

    feat: integrate react query and initial configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,32 @@ import { BrowserRouter } from 'react-router-dom'
 import AppRoutes from './routes/AppRoutes'
 import './App.css'
 import { AlertProvider } from './components/context/AlertProvider'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,  
+      retry: 1,          
+      // If data is older than 5 minutes, it will be reloaded from server
+      staleTime: 1000 * 60 * 5, 
+    },
+  },
+})
 
 function App() {
   return (
-    
+    <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <AlertProvider>
           <AppRoutes />
         </AlertProvider>
       </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
     
   )
 }
